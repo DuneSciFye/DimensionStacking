6ad1b10e3312928d233e917c616a4917feaa74e0
DuneSciFye <65089425+DuneSciFye@users.noreply.github.com>
2024-06-08 00:02:10 -0400

    Yaw, pitch, and velocity now preserved

--- src/main/java/me/dunescifye/dimensionstacking/DimensionStacking.java
+++ src/main/java/me/dunescifye/dimensionstacking/DimensionStacking.java
@@ -5,9 +5,13 @@ import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class DimensionStacking extends JavaPlugin {
-
+    private static DimensionStacking plugin;
+    public static DimensionStacking getPlugin() {
+        return plugin;
+    }
     @Override
     public void onEnable() {
+        plugin = this;
         new VoidListener().voidHandler(this);
         Bukkit.getLogger().info("Plugin enabled.");
 
